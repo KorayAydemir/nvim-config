be0f6096d841814161fc565ef0cfb53603c4442a
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-04 19:17:52 +0300

    supertab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,33 +1,34 @@
 local lsp = require('lsp-zero')
-local ls = require "luasnip"
 lsp.preset('recommended')
+local ls = require "luasnip"
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
 
 lsp.ensure_installed({ 'tsserver', 'eslint', 'lua_ls', 'rust_analyzer' })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    --quickfix
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    lsp.buffer_autoformat()
-    vim.keymap.set({ "i", "s" }, "<Tab>", function() ls.jump(1) end, { desc = "LuaSnip forward jump" })
-    vim.keymap.set({ "i", "s" }, "<S-Tab>", function() ls.jump(-1) end, { desc = "LuaSnip backward jump" })
-end)
-
-lsp.setup()
-
 local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+            fallback();
+        elseif ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+        else
+            fallback()
+        end
+        cmp.mapping.select_next_item()
+    end, { 'i', 's' }),
+})
+
+cmp_mappings['<S-Tab>'] = nil
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
 
 cmp.setup({
     preselect = 'item',
@@ -45,6 +46,28 @@ cmp.setup({
         { name = 'buffer',  keyword_length = 3 },
     }
 })
+
+
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    --quickfix
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set({ "i", "s" }, "<S-Tab>", function() ls.jump(-1) end, { desc = "LuaSnip backward jump" })
+    lsp.buffer_autoformat()
+end)
+
+
+
+lsp.setup()
 
 
 vim.diagnostic.config({

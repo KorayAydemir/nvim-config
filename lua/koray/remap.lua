c0b180fbbe495354e64cbe50b6f8980e6107b12e
Koray Aydemir <korayaydemir276@gmail.com>
2023-08-06 09:21:07 +0300

    remove ultisnips dir

--- lua/koray/remap.lua
+++ lua/koray/remap.lua
@@ -15,11 +15,10 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- next greatest remap ever : asbjornHaland
+-- greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
--- greatest remap ever (mr. primeagen is right)
 vim.keymap.set("x", "<leader>p", [["_dP]]) --paste to void register
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- delete to void register
 
@@ -29,14 +28,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
 vim.keymap.set("n", "<leader>fmk", "<cmd>CellularAutomaton game_of_life<CR>")
 
--- netrwc
---nnoremap('<leader>pv', '<cmd>Lex<CR> :vertical resize 27<CR>')
 ---- telescope
---
 --nnoremap("<leader>pw", function()
 --  require('telescope.builtin').grep_string { search = vim.fn.expand("<cword>") }
 --end)
---
---nnoremap("<C-d>", "<C-d>zz")
---nnoremap("<C-u>", "<C-u>zz")
 --nnoremap('<leader>f', ':Prettier<CR>')

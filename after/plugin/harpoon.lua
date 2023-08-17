8917a7eed7b26ea48c2f4ebc5ad29105ae9df3db
Koray Aydemir <korayaydemir276@gmail.com>
2023-08-17 23:36:34 +0300

    add script for opening persistent terminals

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -9,3 +9,36 @@ vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
 
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+local term = require("harpoon.term")
+local saved_buffer = nil
+local last_term_idx = nil
+local function gotoTerminal(tid, save_buf)
+    saved_buffer = save_buf and vim.fn.bufnr('%') or saved_buffer
+    term.gotoTerminal(tid);
+    last_term_idx = tid
+end
+
+local function isInTerminal()
+    local current_buffer_name = vim.fn.bufname('%')
+    return string.match(current_buffer_name, '^term')
+end
+
+local function toggleTerminal(open, other)
+    if saved_buffer == nil then
+        gotoTerminal(open, true)
+    else
+        if isInTerminal() then
+            if last_term_idx == other then
+                gotoTerminal(open)
+            else
+                vim.cmd('buffer ' .. saved_buffer)
+            end
+        else
+            gotoTerminal(open, true)
+        end
+    end
+end
+
+vim.keymap.set("n", "<C-0>", function() toggleTerminal(1,2) end)
+vim.keymap.set("n", "<C-9>", function() toggleTerminal(2,1) end)

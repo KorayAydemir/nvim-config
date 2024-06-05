6160529d4a83d8af37a9e27f184769ab239e1c18
Koray Aydemir <korayaydemir276@gmail.com>
2024-06-05 23:53:05 +0300

    remove some commented config

--- lua/koray/set.lua
+++ lua/koray/set.lua
@@ -53,9 +53,6 @@ vim.cmd [[autocmd FileType * set formatoptions-=cro]]
 vim.opt.showmode = false
 vim.opt.autoread = true
 
--- code folding
---vim.opt.fillchars = { fold = " " }
---vim.opt.foldenable = false
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 vim.g.markdown_folding = 1

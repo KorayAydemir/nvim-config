12a786ecd57f8c1e439c2e8032ce1bf9a61b5fef
Koray Aydemir <korayaydemir276@gmail.com>
2024-06-29 18:07:35 +0300

    changes

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,22 +1,16 @@
 function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
-	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) 
-    --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
-
-ColorMyPencils("rose-pine")
--- options:
--- fluoromachine ( also has 3 more options in lazy.lua ) 
--- rose-pine
-----------------
+ColorMyPencils("gruvbox")
 
 vim.cmd([[hi Comment guifg='red' ]])
 
 -- Blame line
 vim.g.gitblame_highlight_group = "GitBlame"
-vim.cmd[[hi GitBlame guifg='#aaaaaa' guibg=none]]
+vim.cmd([[hi GitBlame guifg='#aaaaaa' guibg=none]])
 
 -- Status line
 vim.cmd([[hi StatusLine guifg='#fdfdfd' guibg='#a0a0a0']])
-

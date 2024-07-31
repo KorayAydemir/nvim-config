04f15670129b8e840bc9cedf4a835f110e7a4b9e
Koray Aydemir <korayaydemir276@gmail.com>
2024-07-31 09:11:52 +0300

    cord plugin should be disabled on mac

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,10 +2,10 @@ return {
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "folke/twilight.nvim", cmd = "Twilight" },
-	{
-        "vyfor/cord.nvim",
-        build = ".\\build",
-        event = "VeryLazy",
-        opts = {},
-    },
+	--{
+    --    "vyfor/cord.nvim",
+    --    build = ".\\build",
+    --    event = "VeryLazy",
+    --    opts = {},
+    --},
 }

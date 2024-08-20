e0443bd4104aecd8243f3998e2415a5382ef213e
Koray Aydemir <korayaydemir276@gmail.com>
2024-08-20 19:23:50 +0300

    fix formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,96 +1,98 @@
 local function config()
-    local util = require("formatter.util")
+	local util = require("formatter.util")
 
-    local function prettier()
-        return {
-            exe = "prettierd",
-            args = {
-                --"--plugin=prettier-plugin-tailwindcss",
-                "--tab-width=4",
-                util.escape_path(util.get_current_buffer_file_path()),
-            },
-            stdin = true,
-        }
-    end
+	local function prettier()
+		return {
+			exe = "prettierd",
+			args = {
+				--"--plugin=prettier-plugin-tailwindcss",
+				"--tab-width=4",
+				util.escape_path(util.get_current_buffer_file_path()),
+			},
+			stdin = true,
+		}
+	end
 
-    local opts = {
-        logging = true,
-        log_level = vim.log.levels.WARN,
-        filetype = {
-            lua = {
-                require("formatter.filetypes.lua").stylua,
+	local opts = {
+		logging = true,
+		log_level = vim.log.levels.WARN,
+		filetype = {
+			lua = {
+				require("formatter.filetypes.lua").stylua,
 
-                function()
-                    return {
-                        exe = "stylua",
-                        args = {
-                            "--search-parent-directories",
-                            "--stdin-filepath",
-                            util.escape_path(util.get_current_buffer_file_path()),
-                            "--",
-                            "-",
-                        },
-                        stdin = true,
-                    }
-                end,
-            },
-            rust = {
-                function()
-                    return {
-                        exe = "rustfmt",
-                        args = {},
-                        stdin = true,
-                    }
-                end,
-            },
-            tex = {
-                function()
-                    return {
-                        exe = "latexindent",
-                        args = {
-                            "-g",
-                            "/dev/null",
-                        },
-                        stdin = true,
-                    }
-                end,
-            },
-            java = {
-                function()
-                    return {
-                        exe = "google-java-format",
-                        args = {
-                            "--aosp",
-                            util.escape_path(util.get_current_buffer_file_path()),
-                        },
-                        stdin = false,
-                    }
-                end,
-            },
-            c = {
-                require("formatter.filetypes.c").clangformat,
-            },
-            ["js"] = {
-                prettier,
-            },
-            html = { prettier },
-            css = { prettier },
-        },
-    }
+				function()
+					return {
+						exe = "stylua",
+						args = {
+							"--search-parent-directories",
+							"--stdin-filepath",
+							util.escape_path(util.get_current_buffer_file_path()),
+							"--",
+							"-",
+						},
+						stdin = true,
+					}
+				end,
+			},
+			rust = {
+				function()
+					return {
+						exe = "rustfmt",
+						args = {},
+						stdin = true,
+					}
+				end,
+			},
+			tex = {
+				function()
+					return {
+						exe = "latexindent",
+						args = {
+							"-g",
+							"/dev/null",
+						},
+						stdin = true,
+					}
+				end,
+			},
+			java = {
+				function()
+					return {
+						exe = "google-java-format",
+						args = {
+							"--aosp",
+							util.escape_path(util.get_current_buffer_file_path()),
+						},
+						stdin = false,
+					}
+				end,
+			},
+			c = {
+				require("formatter.filetypes.c").clangformat,
+			},
+			javascript = {
+				prettier,
+			},
+			typescript = {
+				prettier,
+			},
+			json = { prettier },
+			html = { prettier },
+			css = { prettier },
+		},
+	}
 
-    require("formatter").setup(opts)
+	require("formatter").setup(opts)
 end
 
-local function format_and_save()
-	vim.cmd("FormatWrite")
-end
+local function format_and_save() vim.cmd("FormatWrite") end
 
 return {
 	"mhartington/formatter.nvim",
-    config = config,
-	keys = {{
+	config = config,
+	keys = { {
 		"<leader>fr",
 		format_and_save,
 		{ silent = true },
-    }},
+	} },
 }
